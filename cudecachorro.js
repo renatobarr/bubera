9e0be7388aaec67f0ac746ab9568ecf3b32e3884
Renato Barreto <renato.barreto@edu.unifil.br>
2024-08-23 00:39:34 -0300

    macaco tocou no meu botão '_'

---
+++ cudecachorro.js
@@ -0,0 +1,10 @@
+function teste(){
+
+    var button = document.getElementById('start');
+    button.classList.remove('pulse');
+    void button.offsetWidth;
+    button.classList.add('pulse');
+
+    
+}
+
